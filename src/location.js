4cf1871e44c3401f09b47ae218399d85c3a7b8bc
bann-dito <ed.bacsierra@gmail.com>
2023-04-06 20:11:42 -0700

    Comment out console logs, add additional comments

--- src/location.js
+++ src/location.js
@@ -1,12 +1,9 @@
 class Location{
     constructor(){
-        //this IP will be gathered from the traceroute body
-        //example api request
-        // this.baseURL = "https://ipgeolocation.abstractapi.com/v1/?api_key=d79e9a495a9847968f527c10331248ee&ip_address="
+
     }
     async getLocation(ip){
         //api call to gather geolocation info
-        // const location = await fetch(`${this.baseURL}${ip}`)
         const location = await fetch(`https://mapping-the-matrix.onrender.com/location?ip=${ip}`)
         const data = await location.json()
         return data
